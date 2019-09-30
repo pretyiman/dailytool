4520077c7e91b0ce878b14ad8f380f9285243113
drnewbie <pretyiman@yahoo.com>
2019-09-30 10:05:40 +0500

    static and wsgi changes

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR =os.path.join(BASE_DIR,'templates')
 STATIC_DIR =os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
+from my_project import MyWSGIApp
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings')
 
 application = get_wsgi_application()
