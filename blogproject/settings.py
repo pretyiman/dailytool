83cbecfd1e735c6befaaebf6e4fb6f5bdf58d17c
drnewbie <pretyiman@yahoo.com>
2019-09-30 10:07:49 +0500

    instaa app adition

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -108,6 +108,7 @@ PASSWORD_HASHERS =[
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
